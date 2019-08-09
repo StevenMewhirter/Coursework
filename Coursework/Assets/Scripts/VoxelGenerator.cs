34a4b38e596ea92c30a30c67cd4f1a90f89c8902
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-08-09 03:08:50 +0100

    i

--- Coursework/Assets/Scripts/VoxelGenerator.cs
+++ Coursework/Assets/Scripts/VoxelGenerator.cs
@@ -14,30 +14,57 @@ public class VoxelGenerator : MonoBehaviour {
     List<int> triIndexList;
     List<Vector2> UVList;
     int numQuads = 0;
+    Dictionary<string, Vector2> texNameCoordDictionary;
 
-    // Use this for initialization
-    void Start() {
+    public List<string> texNames;
+    public List<Vector2> texCoords;
+    public float texSize;
 
+    public void Initialise()
+    {
         mesh = GetComponent<MeshFilter>().mesh;
         meshCollider = GetComponent<MeshCollider>();
         vertexList = new List<Vector3>();
         triIndexList = new List<int>();
         UVList = new List<Vector2>();
-
+  
+        CreateTextureNameCoordDictionary();
         CreateVoxel(0, 0, 0, new Vector2(0, 0));
-      
+    }
+
+    public void UpdateMesh()
+    {
         mesh.vertices = vertexList.ToArray();   // Convert index list to array and store in mesh
         mesh.triangles = triIndexList.ToArray();        // Convert index list to array and store in mesh
         mesh.uv = UVList.ToArray();        // Convert UV list to array and store in mesh
         mesh.RecalculateNormals();
         meshCollider.sharedMesh = null;        // Create a collision mesh
         meshCollider.sharedMesh = mesh;
+    }
 
+    void Start()
+    {
+        
     }
 
     void CreateVoxel(int x, int y, int z, Vector2 uvCoords)
     {
+        CreateNegativeXFace(x, y, z, uvCoords);
+        CreatePositiveXFace(x, y, z, uvCoords);
+        CreateNegativeYFace(x, y, z, uvCoords);
+        CreatePositiveYFace(x, y, z, uvCoords);
         CreateNegativeZFace(x, y, z, uvCoords);
+        CreatePositiveZFace(x, y, z, uvCoords);
+    }
+    void CreateVoxel(int x, int y, int z, string texture)
+    {
+        Vector2 uvCoords = texNameCoordDictionary[texture];
+        CreateNegativeXFace(x, y, z, uvCoords);
+        CreatePositiveXFace(x, y, z, uvCoords);
+        CreateNegativeYFace(x, y, z, uvCoords);
+        CreatePositiveYFace(x, y, z, uvCoords);
+        CreateNegativeZFace(x, y, z, uvCoords);
+        CreatePositiveZFace(x, y, z, uvCoords);
     }
 
     void CreateNegativeZFace(int x, int y, int z, Vector2 uvCoords)
@@ -47,8 +74,121 @@ public class VoxelGenerator : MonoBehaviour {
         vertexList.Add(new Vector3(x + 1, y, z));
         vertexList.Add(new Vector3(x, y, z));
         AddTriangleIndices();
-        AddUVCoords(uvCoords);
+        AddUVCoords(uvCoords);
     }
+    //void CreateNegativeZFace(int x, int y, int z, string texture)
+    //{
+        
+    //    vertexList.Add(new Vector3(x, y + 1, z));
+    //    vertexList.Add(new Vector3(x + 1, y + 1, z));
+    //    vertexList.Add(new Vector3(x + 1, y, z));
+    //    vertexList.Add(new Vector3(x, y, z));
+    //    AddTriangleIndices();
+        
+    //    //  AddUVCoords(uvCoords);
+    //}
+
+    void CreatePositiveZFace(int x, int y, int z, Vector2 uvCoords)
+    {
+
+        vertexList.Add(new Vector3(x + 1, y, z + 1));
+        vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        vertexList.Add(new Vector3(x, y + 1, z + 1));
+        vertexList.Add(new Vector3(x, y, z + 1));
+        AddTriangleIndices();
+        AddUVCoords(uvCoords);
+    }    void CreatePositiveZFace(int x, int y, int z, string texture)
+    {
+        vertexList.Add(new Vector3(x + 1, y, z + 1));
+        vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        vertexList.Add(new Vector3(x, y + 1, z + 1));
+        vertexList.Add(new Vector3(x, y, z + 1));
+        AddTriangleIndices();
+        Vector2 uvCoords = texNameCoordDictionary[texture];
+        //  AddUVCoords(uvCoords);
+    }    void CreateNegativeXFace(int x, int y, int z, Vector2 uvCoords)
+    {
+        vertexList.Add(new Vector3(x, y, z + 1));
+        vertexList.Add(new Vector3(x, y + 1, z + 1));
+        vertexList.Add(new Vector3(x, y + 1, z));
+        vertexList.Add(new Vector3(x, y, z));
+        AddTriangleIndices();
+        AddUVCoords(uvCoords);
+    }
+    void CreateNegativeXFace(int x, int y, int z, string texture)
+    {
+        
+        vertexList.Add(new Vector3(x, y, z + 1));
+        vertexList.Add(new Vector3(x, y + 1, z + 1));
+        vertexList.Add(new Vector3(x, y + 1, z));
+        vertexList.Add(new Vector3(x, y, z));
+        AddTriangleIndices();
+        Vector2 uvCoords = texNameCoordDictionary[texture];
+        // AddUVCoords(uvCoords);
+    }    void CreatePositiveXFace(int x, int y, int z, Vector2 uvCoords)
+        {
+        vertexList.Add(new Vector3(x + 1, y, z));
+        vertexList.Add(new Vector3(x + 1, y + 1, z));
+        vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        vertexList.Add(new Vector3(x + 1, y, z + 1));
+        AddTriangleIndices();
+        AddUVCoords(uvCoords);
+    }
+    void CreatePositiveXFace(int x, int y, int z, string texture)
+    {
+       
+        vertexList.Add(new Vector3(x + 1, y, z));
+        vertexList.Add(new Vector3(x + 1, y + 1, z));
+        vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        vertexList.Add(new Vector3(x + 1, y, z + 1));
+        AddTriangleIndices();
+        Vector2 uvCoords = texNameCoordDictionary[texture];
+        // AddUVCoords(uvCoords);
+    }
+    void CreateNegativeYFace(int x, int y, int z, Vector2 uvCoords)
+    {
+        vertexList.Add(new Vector3(x, y, z));
+        vertexList.Add(new Vector3(x + 1, y, z));
+        vertexList.Add(new Vector3(x + 1, y, z + 1));
+        vertexList.Add(new Vector3(x, y, z + 1));
+        AddTriangleIndices();
+        AddUVCoords(uvCoords);
+    }
+    void CreateNegativeYFace(int x, int y, int z, string texture)
+    {
+        
+        vertexList.Add(new Vector3(x, y, z));
+        vertexList.Add(new Vector3(x + 1, y, z));
+        vertexList.Add(new Vector3(x + 1, y, z + 1));
+        vertexList.Add(new Vector3(x, y, z + 1));
+        AddTriangleIndices();
+        // AddUVCoords(uvCoords);
+        Vector2 uvCoords = texNameCoordDictionary[texture];
+    }
+    void CreatePositiveYFace(int x, int y, int z, Vector2 uvCoords)
+    {
+        
+        vertexList.Add(new Vector3(x + 1, y + 1, z));
+        vertexList.Add(new Vector3(x, y + 1, z));
+        vertexList.Add(new Vector3(x, y + 1, z + 1));
+        vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        AddTriangleIndices();
+        AddUVCoords(uvCoords);
+
+    }
+    void CreatePositiveYFace(int x, int y, int z, string texture)
+    {
+       
+        vertexList.Add(new Vector3(x + 1, y + 1, z));
+        vertexList.Add(new Vector3(x, y + 1, z));
+        vertexList.Add(new Vector3(x, y + 1, z + 1));
+        vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        AddTriangleIndices();
+        texNameCoordDictionary.Add(texNames[i]);
+        // AddUVCoords(uvCoords);
+    }
+
+
 
     void AddTriangleIndices()
     {
@@ -66,6 +206,22 @@ public class VoxelGenerator : MonoBehaviour {
         UVList.Add(new Vector2(uvCoords.x, uvCoords.y + 0.5f));
         UVList.Add(new Vector2(uvCoords.x + 0.5f, uvCoords.y + 0.5f));
         UVList.Add(new Vector2(uvCoords.x + 0.5f, uvCoords.y));
+        UVList.Add(new Vector2(uvCoords.x, uvCoords.y));
+    }
+    void CreateTextureNameCoordDictionary()
+    {
+        texNameCoordDictionary = new Dictionary<string, Vector2>(); // Create a dictionary instance before using
+        if (texNames.Count == texCoords.Count) // Check the number of names and coordinates match
+        {
+            for (int i = 0; i < texNames.Count; i++)// Iterate through both lists
+            {
+                texNameCoordDictionary.Add(texNames[i], texCoords[i]);  // Add the pairing to the dictionary
+            }
+        }
+        else
+        {
+            Debug.Log("texNames and texCoords count mismatch"); // List counts are not matching
+        }
     }
     // Update is called once per frame
     void Update () {
